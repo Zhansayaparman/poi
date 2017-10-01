2df8f86ef48c5740447d0cf799b3574156971bd2
Parman Zhansaya <parmanzhansaya@gmail.com>
2017-10-01 15:35:16 +0600

    Hello.java аздап өзгерді

--- src/as/Hello.java
+++ src/as/Hello.java
@@ -5,5 +5,10 @@ public class Hello {
         System.out.print("Hello World");
         System.out.print("Зертхананың мақсаты");
         System.out.print("Github-ты үйрену");
+
+        int a=MyMath.kvadrat(5);
+        int b=MyMath.kub(5);
+        System.out.print("a="+a);
+        System.out.print("b="+b);
     }
 }
