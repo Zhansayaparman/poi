0d04a7d36142513e10295b2af06567bdb6f57dad
Parman Zhansaya <parmanzhansaya@gmail.com>
2017-10-01 16:46:55 +0600

    MyMath.java файлында isOdd() методына өзгеріс енгіздім.int емес boolean қайтарады.

--- src/as/MyMath.java
+++ src/as/MyMath.java
@@ -21,7 +21,7 @@ public class MyMath {
     }public static int min3(int a,int b,int c){
         int min=min(a,b);
         return min(min,c);
-} public static int isOdd(int a){
+} public static boolean isOdd(int a){
         return (a%2!=0);
 
     }
