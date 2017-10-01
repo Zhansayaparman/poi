ef8808409d3f2dc8c5f4f1056d4722324e03dfc6
Parman Zhansaya <parmanzhansaya@gmail.com>
2017-10-01 15:28:35 +0600

    Екінші коммит

--- src/as/Hello.java
+++ src/as/Hello.java
@@ -3,5 +3,7 @@ package as;
 public class Hello {
     public static void main(String[] args) {
         System.out.print("Hello World");
+        System.out.print("Зертхананың мақсаты");
+        System.out.print("Github-ты үйрену");
     }
 }
